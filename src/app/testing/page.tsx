222ad96254a8b0d8b853332d568cfd2e9636e9bb
Fayash Wahyudha <wahyudhafayash@Fayashs-MacBook-Air.local>
2025-01-17 11:08:26 +0700

    edit previws

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,12 @@
+import { BentoGridDemo } from "./_component/bento";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <BentoGridDemo />
+    </div>
+  );
+};
+
+export default page;
